9df0a6bdd4c233c5cf69a6ef89bf9b50cbff85dc
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-07-01 18:35:56 -0700

    Added finished code from file

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+// Data structure that will store the friends data
+
+var friendsArray = [
+    {
+        name : "Oscar ",
+        photo : "https://www.onthespot.co.uk/wp-content/uploads/2015/08/cute-dog-pictures-for-kids.jpg",
+        scores : [
+            "1",
+			"5",
+			"5",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1",
+			"5"
+
+        ]
+    },
+    {
+        name : "Max",
+        photo : "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1532369199-download-2-1532369181.png",
+        scores : [
+            "2",
+            "3",
+            "1",
+            "5",
+            "2",
+            "4",
+            "2",
+            "4",
+            "1",
+            "4",
+            
+        ]
+    },
+    {
+        name : "Harry",
+        photo : "https://i.pinimg.com/originals/ce/3e/9f/ce3e9fef0577db9f13217b09cdaf1695.jpg",
+        scores :[
+            "3",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1",
+            "2",
+            "1",
+            "5",
+            "2",
+
+        ]
+    }
+]
+
+module.exports = friendsArray;
